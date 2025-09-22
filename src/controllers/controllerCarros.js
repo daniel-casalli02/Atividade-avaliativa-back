1d9ff467a5a007aebd4a93dd64bf6980b0f27c91
Daniel Casalli <danis.settk@gmail.com>
2025-09-22 14:09:24 -0300

    deleteCarro

--- src/controllers/controllerCarros.js
+++ src/controllers/controllerCarros.js
@@ -51,6 +51,35 @@ const getAllCarros = (req, res) => {
 
     carros.push(novoCarro);
 
-    res.status
+    res.status(200).json({
+        success: true,
+        message:"Carro criado com sucesso",
+        carro: novoCarro,
+    })
+
+  }
+
+  const deleteCarro = (req, res) => {
+    const {id} = ew .params;
+    
+    if(isNaN(id)) {
+        res.status(400).json({
+            success:false,
+            message:"O id deve ser válido"
+        });
+    }
+
+    const idParaApagar = parseInt(id);
+
+    const carroParaRemover = carros.find((c) => c.id === idParaApagar);
+
+    if(!carroParaRemover) {
+        res.status(404).json({
+            success:false,
+            message: `O carro com id ${id} não é válido`,
+        });
+    }
+
+    
 
   }
---
+++ src/models/dados.js
@@ -0,0 +1,104 @@
+const carros = [
+    {
+        id: 1,
+        marca: "Ford",
+        modelo: "Fiesta",
+        ano: 2018,
+        cor: "Branco",
+        preco: 45000,
+        combustivel: "Gasolina",
+        quilometragem: 65000
+      },
+      {
+        id: 2,
+        marca: "Chevrolet",
+        modelo: "Onix",
+        ano: 2021,
+        cor: "Preto",
+        preco: 78500,
+        combustivel: "Flex",
+        quilometragem: 22000
+      },
+      {
+        id: 3,
+        marca: "Volkswagen",
+        modelo: "Gol",
+        ano: 2015,
+        cor: "Prata",
+        preco: 32000,
+        combustivel: "Flex",
+        quilometragem: 98000
+      },
+      {
+        id: 4,
+        marca: "Hyundai",
+        modelo: "HB20",
+        ano: 2020,
+        cor: "Vermelho",
+        preco: 62000,
+        combustivel: "Flex",
+        quilometragem: 35000
+      },
+      {
+        id: 5,
+        marca: "Toyota",
+        modelo: "Corolla",
+        ano: 2022,
+        cor: "Cinza",
+        preco: 125000,
+        combustivel: "Híbrido",
+        quilometragem: 15000
+      },
+      {
+        id: 6,
+        marca: "Honda",
+        modelo: "Civic",
+        ano: 2019,
+        cor: "Azul",
+        preco: 90000,
+        combustivel: "Gasolina",
+        quilometragem: 50000
+      },
+      {
+        id: 7,
+        marca: "Fiat",
+        modelo: "Mobi",
+        ano: 2023,
+        cor: "Branco",
+        preco: 69900,
+        combustivel: "Flex",
+        quilometragem: 5000
+      },
+      {
+        id: 8,
+        marca: "Renault",
+        modelo: "Kwid",
+        ano: 2021,
+        cor: "Laranja",
+        preco: 55000,
+        combustivel: "Flex",
+        quilometragem: 28000
+      },
+      {
+        id: 9,
+        marca: "Nissan",
+        modelo: "Kicks",
+        ano: 2020,
+        cor: "Preto",
+        preco: 85000,
+        combustivel: "Gasolina",
+        quilometragem: 40000
+      },
+      {
+        id: 10,
+        marca: "Jeep",
+        modelo: "Renegade",
+        ano: 2017,
+        cor: "Verde",
+        preco: 70000,
+        combustivel: "Diesel",
+        quilometragem: 85000
+      }
+]
+
+export default {carros}
